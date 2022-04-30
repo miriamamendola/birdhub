d147fa48b73f69df943e9b1e9dab6fd1135921d9
Miriam Amendola <m.amendola21@studenti.unisa.it>
2022-04-30 10:08:17 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+###############################################################################
+# Hello Zerynth
+###############################################################################
+
+# Welcome to the first and simplest Zerynth example.
+# Let's just loop forever by printing something to the standard output,
+# in this case, the USB serial port (open the device console to view the output)
+
+# loop forever
+while True:
+    # print something
+    print("Hello Zerynth!")
+    # sleep 1 second
+    sleep(1000)
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+Project created at 2022-04-30T08:04:04.561170Z
+
